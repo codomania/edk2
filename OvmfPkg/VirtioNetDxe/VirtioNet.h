0fda77ea6f02c64c905a54cfb5393a46f7376336
Brijesh Singh <brijesh.singh@amd.com>
2017-09-11 06:32:21 -0500

    OvmfPkg/VirtioNetDxe: alloc RxBuf using AllocateSharedPages()
    When device is behind the IOMMU, VirtioNetDxe is required to use the
    device address in bus master operations. RxBuf is allocated using
    AllocatePool() which returns the system physical address.
    
    The patch uses VIRTIO_DEVICE_PROTOCOL.AllocateSharedPages() to allocate
    the RxBuf and map with VirtioMapAllBytesInSharedBuffer() so that we can
    obtain the device address for RxBuf.
    
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Jordan Justen <jordan.l.justen@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- OvmfPkg/VirtioNetDxe/VirtioNet.h
+++ OvmfPkg/VirtioNetDxe/VirtioNet.h
@@ -4,6 +4,7 @@
   Protocol instances for virtio-net devices.
 
   Copyright (C) 2013, Red Hat, Inc.
+  Copyright (c) 2017, AMD Inc, All rights reserved.<BR>
 
   This program and the accompanying materials are licensed and made available
   under the terms and conditions of the BSD License which accompanies this
@@ -86,6 +87,8 @@ typedef struct {
                                                  // VirtioNetInitRing
   UINT8                       *RxBuf;            // VirtioNetInitRx
   UINT16                      RxLastUsed;        // VirtioNetInitRx
+  UINTN                       RxBufNrPages;      // VirtioNetInitRx
+  VOID                        *RxBufMap;         // VirtioNetInitRx
 
   VRING                       TxRing;            // VirtioNetInitRing
   VOID                        *TxRingMap;        // VirtioRingMap and
@@ -95,6 +98,7 @@ typedef struct {
   UINT16                      *TxFreeStack;      // VirtioNetInitTx
   VIRTIO_1_0_NET_REQ          TxSharedReq;       // VirtioNetInitTx
   UINT16                      TxLastUsed;        // VirtioNetInitTx
+  EFI_PHYSICAL_ADDRESS        RxBufDeviceBase;   // VirtioNetInitRx
 } VNET_DEV;
 
 
